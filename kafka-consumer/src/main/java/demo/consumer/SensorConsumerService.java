ee5ed7ff322f85d9f9887406209da2b37d728f00
kayoung <pgy8404@naver.com>
2026-05-06 01:01:41 +0900

    chore: prometheus kafka로그 수집추가

--- kafka-consumer/src/main/java/demo/consumer/SensorConsumerService.java
+++ kafka-consumer/src/main/java/demo/consumer/SensorConsumerService.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -23,37 +25,39 @@ public class SensorConsumerService {
     @KafkaListener(topics = "sensor.stream", groupId = "display-group", concurrency = "2")
     public void consumeForDisplay(String message,@Header(KafkaHeaders.RECEIVED_PARTITION) int partition){
         log.info("[Display-Group] 파티션: {}, 메시지: {}", partition, message);
-       messagingTemplate.convertAndSend("/topic/sensors", message);
+        messagingTemplate.convertAndSend("/topic/sensors", message);
     }
 
     // group2 : 이상치 알림 전용
     @RetryableTopic(
             attempts = "3",
             backOff = @BackOff(delay = 100,multiplier = 4),
-            dltTopicSuffix = ".storage.dlt"
+            dltStrategy = DltStrategy.FAIL_ON_ERROR,
+            dltTopicSuffix = ".dlt"
     )
 
-    @KafkaListener(topics = "sensor.stream", groupId = "alert-group", concurrency = "2")
-    public void consumeForAlert(String message){
-        try {
-            Thread.sleep(200);
+    @KafkaListener(topics = "sensor.stream", groupId = "alert-group", concurrency = "${kafka.concurrency}")
+    public void consumeForAlert(String message) throws Exception{
 
             SensorData data = SensorData.fromJson(message);
             // 로직: 가속도 합산 벡터가 특정 값 이상이면 위험으로 판단
-            double totalAcc = Math.sqrt(Math.pow(data.getAccX(), 2)
-                    + Math.pow(data.getAccY(), 2)
-                    + Math.pow(data.getAccZ(), 2));
+            double totalAcc = Math.sqrt(
+                    Math.pow(data.getAccX(), 2)
+                            + Math.pow(data.getAccY(), 2)
+                            + Math.pow(data.getAccZ(), 2)
+            );
+            double totalGyro = Math.sqrt(
+                    Math.pow(data.getGyroX(), 2)
+                            + Math.pow(data.getGyroY(), 2)
+                            + Math.pow(data.getGyroZ(), 2)
+            );
+            boolean isShock = totalAcc > 20.0;
+            boolean isRotation = totalGyro > 5.0;
 
-            if (totalAcc > 20.0) {
-                log.warn(" 기기 {}에서 강한 충격 감지: {}", data.getDeviceId(), totalAcc);
-
-                // 알림 전용 토픽으로 별도 전송
+            if (isShock && isRotation) {
+                log.warn(" 낙상 감지 - 기기: {}, 충격: {}, 회전: {}", data.getDeviceId(), totalAcc, totalGyro);
                 messagingTemplate.convertAndSend("/topic/alerts",
-                        "Device " + data.getDeviceId() + ": 위험감지");
+                        "Device " + data.getDeviceId() + ": 낙상 감지");
             }
-        } catch (Exception e) {
-            log.error("알림 분석 에러 발생: {}", e.getMessage());
         }
-
     }
-}
--- kafka-consumer/src/main/java/demo/consumer/SensorDltConsumer.java
+++ kafka-consumer/src/main/java/demo/consumer/SensorDltConsumer.java
@@ -1,4 +1,17 @@
 package demo.consumer;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
 public class SensorDltConsumer {
+    @KafkaListener(topics = "sensor.stream.dlt",groupId ="dlt-group" )
+    public void processDltMessage(String message, @Header(KafkaHeaders.ORIGINAL_TOPIC) String originTopic) {
+        log.error("❌ [DLT 수신] 원본 토픽: {}, 메시지 내용: {}", originTopic, message);
+    }
 }
